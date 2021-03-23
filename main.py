7d4cacd484135ce2bef055fc0338c7397bae0870
tushitjain1 <tushit.jain@sgsdd-students.com>
2021-03-23 10:28:36 +0100

    icon and screen update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from pygame.locals import *
+import sys
+import ctypes
 
 class Calc:
     def __init__(self):
@@ -7,6 +9,7 @@ class Calc:
         self.flags = RESIZABLE
         self.rect = Rect(0, 0, 900, 650)
         Calc.screen = pygame.display.set_mode(self.rect.size, self.flags)
+        pygame.display.flip()
         pygame.display.set_caption("IB equations calculator")
         icon = pygame.image.load("./icons/calc.png")
         pygame.display.set_icon(icon)
@@ -18,12 +21,16 @@ class Calc:
                 if event.type == QUIT:
                     Calc.running = False
                 if event.type == VIDEORESIZE:
-                    screen = pygame.display.set_mode((event.w, event.h), RESIZABLE)
+                    screen = pygame.display.set_mode((event.w, event.h), self.flags)
 
-            Calc.screen.fill(Color('gray'))
+            Calc.screen.fill(Color(105, 105, 105))
             pygame.display.update()
         pygame.quit()
 
 
+
+
 if __name__ == '__main__':
+    appid = 'IBStudents.EquationCalculator.CASProject' # Unique string
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
     Calc().run()
