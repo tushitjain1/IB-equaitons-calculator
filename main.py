b722b653f1c13708a64c4f1fc64e0469593df5d7
NONAME1103 <57779612+NONAME1103@users.noreply.github.com>
2021-04-12 14:16:56 +0200

    Ham menu works

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import ctypes
 import os
 import math
+from sideMenu import SideMenu, REDRAW_WINDOW
 pygame.font.init()
 
 # Default window sizes # Other possible sizes? idrk yet
@@ -16,12 +17,14 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("IB equations calculator")
 
 # Colours
-MAIN_COLOUR = (133,220,186)
+MAIN_COLOUR = (133, 220, 186)
 SETTINGS_COLOUR = (226, 125, 96)
-FRAME_COLOUR = (232,168,124)
+FRAME_COLOUR = (232, 168, 124)
 OPTION_COLOUR = (50, 50, 50)
-CALC_BACKROUND_COLOUR = (214,220,240)
+CALC_BACKROUND_COLOUR = (214, 220, 240)
 CALC_FRAME_COLOUR = (94, 92, 84)
+COLOR_LIST_INACTIVE = (255, 100, 100)
+COLOR_LIST_ACTIVE = (255, 150, 150)
 
 # Fonts
 BASICFONT = pygame.font.SysFont("Arial", 30)
@@ -30,7 +33,7 @@ TITLE2FONT = pygame.font.SysFont("Arial", 40)
 TITLEFONT2 = pygame.font.SysFont("Arial", 15)
 
 # Other variables
-FPS = 60
+FPS = 120
 
 # Load images
 ICON = pygame.image.load(os.path.join('icons', 'calc.png'))
@@ -39,23 +42,37 @@ SETTINGS = pygame.transform.smoothscale(SETTINGS, (40, 40))
 HAMTHREE = pygame.image.load(os.path.join('icons', 'hamthree.svg'))
 HAMTHREE = pygame.transform.smoothscale(HAMTHREE, (50, 50))
 CALCIMAGE = pygame.image.load(os.path.join('icons', 'calculatorart-01.png'))
-CALCIMAGE = pygame.transform.smoothscale(CALCIMAGE, (390,500))
+CALCIMAGE = pygame.transform.smoothscale(CALCIMAGE, (390, 500))
 
 # Set window icon
 pygame.display.set_icon(ICON)
+
+# Hamburger menu
+option_rect = (0, 10, 200, 90)
+ham_pos = (20, 10)
+opts = ["Equation %i" %i for i in range(15)]
+ham = SideMenu(
+    [(0, 0, 0), (0, 0, 0)],
+    [COLOR_LIST_INACTIVE, COLOR_LIST_ACTIVE],
+    option_rect,
+    BASICFONT,
+    HAMTHREE, ham_pos, opts)
+hamSurf = pygame.Surface((option_rect[2]+20, option_rect[3]*len(opts) + HEIGHT/2))
 
 
 def main():
     clock = pygame.time.Clock()
     run = True
+    scroll_y = HAMTHREE.get_height()
     drawWindow()
-    show_settings = False
     numericString = ""
+    show_settings = False
     while run:
         clock.tick(FPS)
         x, y = WIN.get_size()
         pygame.display.update()
-        for event in pygame.event.get():
+        event_list = pygame.event.get()
+        for event in event_list:
             if event.type == pygame.QUIT:
                 run = False
             if event.type == MOUSEBUTTONDOWN:
@@ -76,74 +93,86 @@ def main():
                 elif pygame.Rect(0, 0, x, y).collidepoint(event.pos) and show_settings:
                         drawWindow()
                         show_settings = False
+                        hamSurf.set_alpha(100)
                 # If click on a rect exactly the same as HAMTHREE icon, draw equations menu
-                if pygame.Rect(20, 10, 50, 50).collidepoint(event.pos):
-                    print("HamThree")
-                    drawHam()
+                if event.button == 4 and ham.draw_menu: scroll_y = min(scroll_y + 15, 50)
+                if event.button == 5 and ham.draw_menu: scroll_y = max(scroll_y - 15, -(option_rect[3]*len(opts)) + (y-(ham_pos[1] + HAMTHREE.get_height())))
                 #If the click was within a rectangle at the same position as the caclulator png
-                if pygame.Rect(x-312, y- 445, 275, 410).collidepoint(event.pos) and not show_settings:
+                if pygame.Rect(x-312, y-445, 275, 410).collidepoint(event.pos) and not show_settings:
                     #checking individual buttons
-                    if pygame.Rect(x-280,y-298,40,40).collidepoint(event.pos): #7
+                    if pygame.Rect(x-280, y-298, 40, 40).collidepoint(event.pos): #7
                         numericString += "7"
-                    elif pygame.Rect(x-221,y-298,40,40).collidepoint(event.pos): #8
+                    elif pygame.Rect(x-221, y-298, 40, 40).collidepoint(event.pos): #8
                         numericString += "8"
-                    elif pygame.Rect(x-162,y-298,40,40).collidepoint(event.pos): #9
+                    elif pygame.Rect(x-162, y-298, 40, 40).collidepoint(event.pos): #9
                         numericString += "9"
-                    elif pygame.Rect(x-103,y-298,40,40).collidepoint(event.pos): #plus
+                    elif pygame.Rect(x-103, y-298, 40, 40).collidepoint(event.pos): #plus
                         numericString += "+"
-                    elif pygame.Rect(x-280,y-238,40,40).collidepoint(event.pos): #4
+                    elif pygame.Rect(x-280, y-238, 40, 40).collidepoint(event.pos): #4
                         numericString += "4"
-                    elif pygame.Rect(x-221,y-238,40,40).collidepoint(event.pos): #5
+                    elif pygame.Rect(x-221, y-238, 40, 40).collidepoint(event.pos): #5
                         numericString += "5"
-                    elif pygame.Rect(x-162,y-238,40,40).collidepoint(event.pos): #6
+                    elif pygame.Rect(x-162, y-238, 40, 40).collidepoint(event.pos): #6
                         numericString += "6"
-                    elif pygame.Rect(x-103,y-238,40,40).collidepoint(event.pos): #minus
+                    elif pygame.Rect(x-103, y-238, 40, 40).collidepoint(event.pos): #minus
                         numericString += "-"
-                    elif pygame.Rect(x-280,y-178,40,40).collidepoint(event.pos): #1
+                    elif pygame.Rect(x-280, y-178, 40, 40).collidepoint(event.pos): #1
                         numericString += "1"
-                    elif pygame.Rect(x-221,y-178,40,40).collidepoint(event.pos): #2
+                    elif pygame.Rect(x-221, y-178, 40, 40).collidepoint(event.pos): #2
                         numericString += "2"
-                    elif pygame.Rect(x-162,y-178,40,40).collidepoint(event.pos): #3
+                    elif pygame.Rect(x-162, y-178, 40, 40).collidepoint(event.pos): #3
                         numericString += "3"
-                    elif pygame.Rect(x-103,y-178,40,40).collidepoint(event.pos): #times
+                    elif pygame.Rect(x-103, y-178, 40, 40).collidepoint(event.pos): #times
                         numericString += "*"
-                    elif pygame.Rect(x-280,y-118,40,40).collidepoint(event.pos): #0
+                    elif pygame.Rect(x-280, y-118, 40, 40).collidepoint(event.pos): #0
                         numericString += "0"
-                    elif pygame.Rect(x-221,y-118,40,40).collidepoint(event.pos): #point
+                    elif pygame.Rect(x-221, y-118, 40, 40).collidepoint(event.pos): #point
                         numericString += "."
-                    elif pygame.Rect(x-162,y-118,40,40).collidepoint(event.pos): #equals to
+                    elif pygame.Rect(x-162, y-118, 40, 40).collidepoint(event.pos): #equals to
                         print(calculator(numericString))
                         numericString = ""
-                    elif pygame.Rect(x-103,y-118,40,40).collidepoint(event.pos): #divide
+                    elif pygame.Rect(x-103, y-118, 40, 40).collidepoint(event.pos): #divide
                         numericString += "/"
-                    elif pygame.Rect(x-281,y-66,30,25).collidepoint(event.pos): #pi
+                    elif pygame.Rect(x-281, y-66, 30, 25).collidepoint(event.pos): #pi
                         numericString += str(math.pi)
-                    elif pygame.Rect(x-235,y-66,30,25).collidepoint(event.pos): #to the power
+                    elif pygame.Rect(x-235, y-66, 30, 25).collidepoint(event.pos): #to the power
                         numericString += "**"
-                    elif pygame.Rect(x-187,y-66,30,25).collidepoint(event.pos): #*10^y
+                    elif pygame.Rect(x-187, y-66, 30, 25).collidepoint(event.pos): #*10^y
                         numericString += "*10**"
-                    elif pygame.Rect(x-142,y-66,30,25).collidepoint(event.pos): #open bracket
+                    elif pygame.Rect(x-142, y-66, 30, 25).collidepoint(event.pos): #open bracket
                         numericString += "("
-                    elif pygame.Rect(x-93,y-66,30,25).collidepoint(event.pos): #close bracket
+                    elif pygame.Rect(x-93, y-66, 30, 25).collidepoint(event.pos): #close bracket
                         numericString += ")"
-                    elif pygame.Rect(x-274,y-414,32,22).collidepoint(event.pos): #close bracket
+                    elif pygame.Rect(x-274, y-414, 32, 22).collidepoint(event.pos): #clear
                         numericString = ""
+
+            if event.type == REDRAW_WINDOW: drawWindow()
+        selected_option = ham.update(event_list, scroll_y)
+        if selected_option >= 0:
+            opt = ham.options[selected_option]
+            print(opt)
+        ham.draw(hamSurf)
+        WIN.set_clip((0, ham_pos[1] + HAMTHREE.get_height(), x, y-(ham_pos[1] + HAMTHREE.get_height())))
+        WIN.blit(hamSurf, hamSurf.get_rect(top=scroll_y))
+        WIN.set_clip(None)
 
     pygame.quit()
 
-# Draw the main windows
+# Draw the main window
 def drawWindow():
     x, y = WIN.get_size()
     WIN.fill(MAIN_COLOUR)
+    hamSurf.fill(MAIN_COLOUR)
     WIN.blit(SETTINGS, (x-50, 10))
     WIN.blit(HAMTHREE, (20, 10))
     WIN.blit(CALCIMAGE, (x-375, y- 500))
-    clear = TITLEFONT2.render("Clear",1,(0,0,0))
-    WIN.blit(clear,(x-274,y-414))
+    clear = TITLEFONT2.render("Clear", 1, (0, 0, 0))
+    WIN.blit(clear, (x-274, y-414))
     pygame.display.update()
 
 # Draw the settings window
 def drawSettings(x, y):
+    hamSurf.set_alpha(0)
     settings = pygame.Rect(70, 85, x-140, y-170)
     frame = pygame.Rect(75, 90, x-150, y-180)
     pygame.draw.rect(WIN, SETTINGS_COLOUR, settings, 0)
@@ -180,9 +209,10 @@ def drawHam():
 
 # Changes the global variables to the new desired size
 def resize(size):
-    global WIDTH, HEIGHT, WIN
+    global WIDTH, HEIGHT, WIN, hamSurf
     WIDTH, HEIGHT = size
     WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+    hamSurf = pygame.Surface((option_rect[2]+20, option_rect[3]*len(opts) + HEIGHT/2))
     drawWindow()
     drawSettings(WIDTH, HEIGHT)
 
