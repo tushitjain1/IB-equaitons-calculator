fb2545228b41112a768e7aab90f7f7b4e9137e8d
NONAME1103 <faparicio1103@gmail.com>
2021-09-29 20:15:11 +0200

    Big Progress

--- hiddenWebDev/webdev.py
+++ hiddenWebDev/webdev.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template,request
 
 app = Flask(__name__)
-@app.route('/', methods=['POST','GET'])
+
+
+@app.route('/', methods=['POST', 'GET'])
 def home():
     val = 0
-    pics = ['para1','tri1']
+    pics = ['para1', 'tri1', 'trap1', 'circ1', 'cyl1', 'circ2', 'cub1', 'cyl2', 'pris1', 'poi1', 'poi2', 'term1',
+            'term2', 'sum1', 'sum2', 'sum3', 'int1', 'comb1', 'perm1', 'gra1', 'axi1', 'quad1', 'disc1', 'poi3',
+            'poi4', 'pyr1', 'con1', 'sph1', 'con2', 'sph2', 'sin1', 'cos1', 'tri1', 'arc1', 'sec1', 'mag1',
+            'dot1', 'ang1']
     if request.method == 'GET':
         val = 0
     if request.method == 'POST':
-        if request.form['para1']:
-            val = 1
-        print('works')
-    return render_template("home.html", val = val)
+        for k in request.form.keys():
+            print(k)
+            val = pics.index(k) + 1
+            print(val)
+    return render_template("home.html", val=val)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
