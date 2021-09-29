75d1ca20b8a90bc5f47b7a17a13320a75eac1164
Franco Aparicio <faparicio1103@gmail.com>
2021-09-29 17:22:49 +0200

    jinja confusion

--- hiddenWebDev/__init__.py
+++ hiddenWebDev/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 
+
 def createApp():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'computerscienceIA'
-    
+
     from .views import views
-    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
     return app
--- hiddenWebDev/views.py
+++ hiddenWebDev/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template 
+from flask import Blueprint, render_template
 
-views = Blueprint('views',__name__)
+views = Blueprint('views', __name__)
+
 
 @views.route('/')
 def home():
-    return render_template("equations.html")  
+    return render_template("equations.html")
