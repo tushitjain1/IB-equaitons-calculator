aec1c69bda5199d5bc8ea59e6f1a60595a40b2c4
Franco Aparicio <faparicio1103@gmail.com>
2021-10-06 15:28:21 +0200

    keep going

--- hiddenWebDev/webdev.py
+++ hiddenWebDev/webdev.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template,request
 app = Flask(__name__)
+val = 0
 
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    val = 0
+    global val
     pics = ['para1', 'tri1', 'trap1', 'circ1', 'cyl1', 'circ2', 'cub1', 'cyl2', 'pris1', 'poi1', 'poi2', 'term1',
             'term2', 'sum1', 'sum2', 'sum3', 'int1', 'comb1', 'perm1', 'gra1', 'axi1', 'quad1', 'disc1', 'poi3',
             'poi4', 'pyr1', 'con1', 'sph1', 'con2', 'sph2', 'sin1', 'cos1', 'tri1', 'arc1', 'sec1', 'mag1',
             'dot1', 'ang1']
-    if request.method == 'GET':
-        val = 0
+    # if request.method == 'GET':
+    #     val = 0
+    print(val)
     if request.method == 'POST':
         for k in request.form.keys():
-            val = pics.index(k) + 1
+            if k in pics:
+                val = pics.index(k) + 1
+            else:
+                print("Calc-text:", request.form[k])
     return render_template("home.html", val=val)
 
 
