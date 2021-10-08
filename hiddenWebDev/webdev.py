563b79787b0e40a9aa5cf69e1f52e6cd02b18c58
tushitjain1 <tushit.jain@sgsdd-students.com>
2021-10-08 11:53:05 +0200

    pics done

--- hiddenWebDev/webdev.py
+++ hiddenWebDev/webdev.py
@@ -10,7 +10,7 @@ def home():
     calcText = ""
     pics = ['icon', 'para1', 'tri1', 'trap1', 'circ1', 'cyl1', 'circ2', 'cub1', 'cyl2', 'pris1', 'poi1', 'poi2',
             'term1', 'term2', 'sum1', 'sum2', 'sum3', 'int1', 'comb1', 'perm1', 'gra1', 'axi1', 'quad1', 'disc1',
-            'poi3', 'poi4', 'pyr1', 'con1', 'sph1', 'con2', 'sph2', 'sin1', 'cos1', 'tri1', 'arc1', 'sec1', 'mag1',
+            'poi3', 'poi4', 'pyr1', 'con1', 'sph1', 'con2', 'sph2', 'sin1', 'cos1', 'tri2', 'arc1', 'sec1', 'mag1',
             'dot1', 'ang1']
     if request.method == 'POST':
         for k in request.form.keys():
