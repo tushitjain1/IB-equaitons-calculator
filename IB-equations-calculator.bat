c78b3a27d2be9b902542d818561f40bdfe7cc91c
NONAME1103 <57779612+NONAME1103@users.noreply.github.com>
2021-10-13 16:58:06 +0200

    Add files via upload

---
+++ IB-equations-calculator.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+setlocal
+set PATH=.\venv\Scripts\
+@REM
+ECHO Close this window to stop the program
+ECHO Note: The web page will no longer load new information if the program is stopped
+python.exe webdev.py
---
+++ IB-equations-calculator.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PATH=$(pwd)/venv/bin/
+echo Close this window to stop the program
+echo Note: The web page will no longer load new information if the program is stopped
+python webdev.py
