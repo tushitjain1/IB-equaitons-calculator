e962c9e0a71607e346213b2ff71e7299a09e614d
NONAME1103 <57779612+NONAME1103@users.noreply.github.com>
2021-10-13 18:32:07 +0200

    Batch works!

--- IB-equations-calculator.bat
+++ IB-equations-calculator.bat
@@ -1,7 +1,24 @@
 @ECHO OFF
-setlocal
-set PATH=.\venv\Scripts\
-@REM
+
+python --version 3.8>NUL
+IF ERRORLEVEL 1 GOTO NOPYTHON
+
+IF NOT EXIST .\venv\ (
+  python -m venv venv
+  setlocal
+  set PATH=.\venv\Scripts\
+  @REM
+  python -m pip install -r requirements.txt -qqq --disable-pip-version-check
+) ELSE (
+  setlocal
+  set PATH=.\venv\Scripts\
+  @REM
+)
+
 ECHO Close this window to stop the program
 ECHO Note: The web page will no longer load new information if the program is stopped
 python.exe webdev.py
+GOTO:EOF
+
+:NOPYTHON
+ECHO Please install Python 3.8 or greater to run this program
